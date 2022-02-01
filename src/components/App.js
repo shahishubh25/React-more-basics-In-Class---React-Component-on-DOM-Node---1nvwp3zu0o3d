4c1f3bf413b75efb666a2565ab0e6e0ec994f448
Shubham Shahi <shubhamshahi552@gmail.com>
2022-02-01 23:23:04 +0530

    Assignment 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
+
 function App() {
   return (
-    <>
-      <p>
+    <p>
         Now I can render any React component on any DOM node I want using
         ReactDOM.render
       </p>
-    </>
-  );
+   );
 }
 export default App;
